abf7f8109b6f7a7ddb181f961b325e302889cf7d
raniafff <rania.tsabitah@ui.ac.id>
2026-09-02 16:58:22 +0700

    perbaiki link pws

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-#1tr8b@lqnud^7s3yfjhgu6owi@b#$lnr962)2&q1+f^xk%!a%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://rania-tsabitah-myportofolio.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rania-tsabitah-myportofolio.pws.cs.ui.ac.id"]
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
