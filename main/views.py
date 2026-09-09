b24e7de17a5d20bd1bd28476ad420e24d095995a
raniafff <rania.tsabitah@ui.ac.id>
2026-09-09 17:28:55 +0700

    bikin show_main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Rania Tsabitah Firsa",
+        "nickname": "Naea",
+        "npm": "2506616693",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Certified daydreamer. Living proof that you can major in something "
+            "longer than planned while keeping your sanity intact."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Rania Tsabitah Firsa",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
