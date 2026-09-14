cba52e7c96c3d9d0fa5756fd55ff6251d974a2ca
raniafff <rania.tsabitah@ui.ac.id>
2026-09-14 21:39:02 +0700

    nambah hobby di main

--- main/models.py
+++ main/models.py
@@ -24,3 +24,13 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Hobby(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    frequency = models.CharField(max_length=100)  # Contoh: "Setiap hari", "Akhir pekan"
+    started_since = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
