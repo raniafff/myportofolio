2294aa43c5dfd456ee84a6704c97baa175ebd0f6
raniafff <rania.tsabitah@ui.ac.id>
2026-09-16 17:32:37 +0700

    menyelesaikan langkah 1

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 from main.models import Experience, Hobby
+from main.forms import ProjectForm
 
 
 def show_main(request):
@@ -30,3 +34,17 @@ def show_hobbies(request):
         "hobby_list": Hobby.objects.all(),
     }
     return render(request, "hobbies.html", context)
+
+def create_project(request):
+    form = ProjectForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Proyek baru berhasil ditambahkan!")
+        return redirect("main:show_projects")
+
+    context = {
+        "name": "Burhan",
+        "form": form,
+    }
+    return render(request, "projects_form.html", context)
