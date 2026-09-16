91425128972f1a139ddede8c28b1c5c035bf2fdc
raniafff <rania.tsabitah@ui.ac.id>
2026-09-16 23:00:06 +0700

    bismillah ga error

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, create_project, show_projects, get_projects_json 
+from main.views import show_main, show_experience, create_project, show_project, get_projects_json 
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
-    path("projects/", show_projects, name="show_projects"), 
+    path("projects/", show_project, name="show_project"), 
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
 ]
