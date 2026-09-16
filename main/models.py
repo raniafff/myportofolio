fc2a9e1d05d76f1328c7f2261fece4d1e97e08c4
raniafff <rania.tsabitah@ui.ac.id>
2026-09-16 20:54:46 +0700

    nambah models yg ketinggalan

--- main/models.py
+++ main/models.py
@@ -34,3 +34,14 @@ class Hobby(models.Model):
 
     def __str__(self):
         return self.name
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tech_stack = models.CharField(max_length=255)
+    project_url = models.URLField(blank=True)
+    project_image_url = models.URLField(blank=True, max_length=500)
+
+    def __str__(self):
+        return self.title
